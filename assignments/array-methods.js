2bc2ebbedf4a9d436c4b26f0065fcbdc8006233b
nevets615 <surj316@yahoo.com>
2019-01-22 18:11:47 -0500

    saved array

--- assignments/array-methods.js
+++ assignments/array-methods.js
@@ -68,14 +68,11 @@ console.log(fullName);
 
 let allCaps = [];
 
-
-const allCaps = first_name.map( function(currentValue){
-  return currentValue.state.toUpperCase();
+const allCap = runners.map( function(currentValue){
+    return currentValue.first-name.toUpperCase();
+  
 });
-
-
-
-console.log(allCaps); 
+  
 
 // ==== Challenge 3: Use .filter() ====
 // The large shirts won't be available for the event due to an ordering issue.  Get a list of runners with large sized shirts so they can choose a different size. Return an array named largeShirts that contains information about the runners that have a shirt size of L and log the result
